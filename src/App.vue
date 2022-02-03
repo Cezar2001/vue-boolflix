b01ec28c31dc70d5f954f088cff01a4b8ad883b0
Cezar <cezarlantosgmail.com>
2022-02-03 15:27:17 +0100

    milestone 1

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,7 @@ export default {
   data() {
     return{
       filmList:[],
+      filmListFiltered:[],
     }
   },
   methods: {
@@ -26,10 +27,17 @@ export default {
       let url = "https://api.themoviedb.org/3/search/movie?query=";
       let keyword = "@search"
       let apiId = "&api_key=f655c752493edcdf5f88d9b75b5667fd"
-      const link = axios.get(url + keyword + apiId)
+        const link = axios.get(url + keyword + apiId).then((results) => {
+        this.filmList = results.data  
+        this.filmListFiltered = results.data  
+      })
       this.filmList.push(link);
       // return link.title
-    }
+      console.log(link.title)
+    },
+  //   stampFilm() {
+  //     this.filmListFiltered = this.filmList.
+  //   }
   }
 }
 </script>
