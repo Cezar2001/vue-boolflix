b1f9880b5da86025202fa77cf274667e8d6e5aad
Cezar <cezarlantosgmail.com>
2022-02-04 18:21:05 +0100

    popular film problem

---
+++ src/components/PopularTrend.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div
+        v-for="popular in popularFilms"
+        :key="popular.id"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        popularFilms: Array
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
